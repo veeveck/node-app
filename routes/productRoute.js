0ef58eb769d7d1e5a7284c6dc746d3b67d122baa
Vivek <veeveckjeet@gmail.com>
2023-04-24 13:38:59 -0400

    Change to MVC pattern

---
+++ routes/productRoute.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const productController=require('../controller/product');
+const router=express.Router();
+router
+.post('/',productController.createProduct)
+.get('/',productController.getAllProducts)
+.get('/:id',productController.getProduct)
+.put('/:id',productController.replaceProduct)
+.patch('/:id',productController.updateProject)
+.delete('/:id',productController.deleteProduct);
+
+exports.routes=router;
